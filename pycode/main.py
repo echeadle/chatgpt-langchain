4f8317c506ee7705a26e033560423970ff2d80a6
echeadle <echeadle@gmail.com>
2023-10-29 19:35:14 -0600

    Lesson 11

--- pycode/main.py
+++ pycode/main.py
@@ -1,6 +1,8 @@
 import os
 from dotenv import find_dotenv, load_dotenv
 from langchain.llms import OpenAI
+from langchain.prompts import PromptTemplate
+from langchain.chains import LLMChain
 
 
 load_dotenv(find_dotenv())
@@ -10,6 +12,20 @@ llm = OpenAI(
         openai_api_key=api_key
 )
 
-result = llm("Write a very very short poem")
-print(result)
+code_prompt = PromptTemplate(
+    template="Write a very short {language} function that will {task}",
+    input_variables=["language", "task"]
+)
+
+code_chain = LLMChain(
+    llm=llm,
+    prompt=code_prompt
+)
+
+result = code_chain({
+    "language": "python",
+    "task": "return a list of numbers"
+})
+
+print(result["text"])
 
